3d8ddd993cbd852f74b77b1381f66d3140fff735
ramonito omandam <ram.omandam@gmail.com>
2026-01-22 15:28:34 +0800

    finished my portfolio design using gemini prompt

--- js/app.js
+++ js/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#000000"
+        "value": "#b41c1c"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#a81313"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#29298c",
+        "color": "#ffffff",
         "opacity": 0.4,
         "width": 1
       },
